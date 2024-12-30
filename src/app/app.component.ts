59a68ee850098befc6592f73af3f50b38289a1ea
José <js942650@gmail.com>
2024-12-29 21:44:37 -0300

    proejto quase finalizado, exceto lógica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ InputMaskModule,InputTextModule, FloatLabelModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'CreditCard-Validation';
+    text: string = ''; // Variável para armazenar o texto digitado
 }
