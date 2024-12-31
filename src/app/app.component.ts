56e270500a33be3096d63b9cd1602c2272fa4443
José <js942650@gmail.com>
2024-12-30 21:54:20 -0300

    falta apenas a validação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,38 @@ import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ InputMaskModule,InputTextModule, FloatLabelModule,FormsModule],
+  imports: [ InputMaskModule,InputTextModule, FloatLabelModule,FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'CreditCard-Validation';
-    text: string = ''; // Variável para armazenar o texto digitado
+  nome: string = ""; // Declaração da propriedade 'text'
+  numero:any | null = null;
+  mm:number | null = null;
+  yy:number | null = null;
+  cvc:number | null = null;
+
+  constructor() {
+    
+  } 
+  formatarComEspacos(input: string): string {
+    // Remove todos os espaços e formata a string com espaços a cada 4 caracteres
+    return input.replace(/\D/g, '') // Remove tudo o que não for número
+                .replace(/(\d{4})(?=\d)/g, '$1 '); 
+    // Adiciona espaço a cada 4 dígitos
+  }
+  
+  atualizarFormat(event: any): void {
+    const numero = event.target.value;
+    const valorFormatado = this.formatarComEspacos(numero);
+    this.numero = valorFormatado;
+  }
 }
